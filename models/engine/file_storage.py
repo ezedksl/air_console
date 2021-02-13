3e73771d279ad5e4d733475af07df1e02e2fd34b
Lunerio <lunerio@outlook.es>
2021-02-13 17:48:41 -0300

    saVE

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,12 +13,12 @@ class FileStorage():
     deserializes from a file
     """
     __file_path = "file.json"
-    __objects: {}
+    __objects = {}
 
     def all(self):
         """return the __objects dictionary
         """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """set the __objects with obj in the key
@@ -35,17 +35,10 @@ class FileStorage():
         new_dict = {}
         for key, value in FileStorage.__objects.items():
             new_dict[key] = value.to_dict()
-            #mucha discusión por acá
+            #mucha discusión por acá si va el self o no
 
         with open(FileStorage.__file_path, 'w') as f:
             json.dump(new_dict, f)
-
-"""{"BaseModel.ee49c413-023a-4b49-bd28-f2936c95460d":
-    {"my_number": 89, "__class__":"BaseModel",
-     "updated_at": "2017-09-28T21:07:25.047381",
-     "created_at": "2017-09-28T21:07:25.047372",
-     "name": "Holberton",
-     "id": "ee49c413-023a-4b49-bd28-f2936c95460d"}}"""
 
     def reload(self):
         """ Deserialize the JSON file to __objects (only if JSON file exists).
@@ -56,8 +49,9 @@ class FileStorage():
                 all_dicts = json.load(f)
             for key, value in all_dicts.items():
                 # Loop through dictionary of dictionaries,
-                # Creating an
+                # Creating an instance of the class for each dictionary.
                 inst = value.__class__(value)
+                # Save new instance in __objects.
                 self.new(inst)
         except:
             pass
